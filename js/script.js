92f9435e2615d997df3ed3256e4f10e9b27f0668
Estefany Serrano Vega <svegaestefany@gmail.com>
2022-09-18 14:55:04 -0400

    actualizacion menu responsive

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-var btn = document.getElementById('btn-header');
-if (btn) { alert('funciona'); }
+document.getElementById("menu-bars").addEventListener("click", funciona);
+var menu = document.getElementById("menu-desplegable");
+
+function funciona() {
+    menu.classList.toggle("menu-bars_active");
+}
